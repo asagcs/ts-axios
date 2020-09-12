5576c162c3a6fa6c4719866e52919a62171f854f
李东升 <jj15838317072@163.com>
2020-09-12 16:38:01 +0800

    错误处理

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,14 +1,19 @@
 import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types';
 import { parseHeaders } from './helpers/headers';
+import { createError } from './helpers/error';  
 
 export default function xhr (config: AxiosRequestConfig): AxiosPromise {
-    return new Promise((resolve) => {
-        const { data = null, url, method = 'get', headers, responseType } = config
+    return new Promise((resolve, reject) => {
+        const { data = null, url, method = 'get', headers, responseType, timeout } = config
   
         const request = new XMLHttpRequest()
 
         if(responseType) {
             request.responseType = responseType;
+        }
+
+        if(timeout) {
+            request.timeout = timeout;
         }
     
         request.open(method.toUpperCase(), url, true)
@@ -16,6 +21,10 @@ export default function xhr (config: AxiosRequestConfig): AxiosPromise {
         request.onreadystatechange = function handleLoad() {
             if(request.readyState !== 4) {
                 return 
+            }
+
+            if(request.status === 0) {
+                return;
             }
 
             const responseHeaders = parseHeaders(request.getAllResponseHeaders());
@@ -29,16 +38,32 @@ export default function xhr (config: AxiosRequestConfig): AxiosPromise {
                 request
             }
 
-            resolve(response);
+            handleResponse(response);
+        }
+
+        request.onerror = function handleError() {
+            reject(createError('Network Error', config, null, request))
+        }
+
+        request.ontimeout = function handleTimeout() {
+            reject(createError(`Timeout of ${timeout} ms exceeded`, config, 'ECONNABORTED', request))
         }
     
         Object.keys(headers).forEach((name) => {
-        if (data === null && name.toLowerCase() === 'content-type') {
-            delete headers[name]
-        } else {
-            request.setRequestHeader(name, headers[name])
-        }
+            if (data === null && name.toLowerCase() === 'content-type') {
+                delete headers[name]
+            } else {
+                request.setRequestHeader(name, headers[name])
+            }
         })
+
+        function handleResponse(response: AxiosResponse) {
+            if(response.status >= 0 && response.status <300) {
+                resolve(response);
+            }else{
+                reject(createError(`Requset faild with status code ${response.status}`, config, null, request, response))
+            }
+        }
     
         request.send(data)
     })
