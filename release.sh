889cb68f3c6b515654816dca1ca220a755db23ae
李东升 <jj15838317072@163.com>
2020-11-16 08:52:44 +0800

    [build] 0.0.1

--- release.sh
+++ release.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env sh
 set -e
-
 echo "Enter release version: "
 read VERSION
-read -p "Release $VERSION -are you sure?(y/n)" -n 1 -r
-echo #(optional) move to a new line
-if [[ $RELY =~ ^[Y/y]$ ]]
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo  # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    echo "Release $VERSION ..."
-    
-    #commit
-    git add -A
-    git commit -m "[build] $VERSION"
-    npm version $VERSION --message "[release]  $VERSION"
-    git push origin master
+  echo "Releasing $VERSION ..."
 
-    #publish
-    npm publish
+  # commit
+  git add -A
+  git commit -m "[build] $VERSION"
+  npm version $VERSION --message "[release] $VERSION"
+  git push origin master
+
+  # publish
+  npm publish
 fi
